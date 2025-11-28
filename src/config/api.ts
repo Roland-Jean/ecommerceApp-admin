2008ed961d2e73aefa1a20981a362896a2b07016
Herve176 <hdjomo1@gmail.com>
2025-11-28 09:16:26 +0100

    fix: increase axios timeout to 100000000

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:8081/api/v1",
-  timeout: 10000,
+  timeout: 100000000,
   headers: { "X-Custom-Header": "foobar" },
 });
