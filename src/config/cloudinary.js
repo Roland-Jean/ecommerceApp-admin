8319bc3fe801253517929c14b7006b622365bc4b
herve <hdjomo1@gmail.com>
2025-12-30 03:47:44 +0100

    feat: Integrate Cloudinary for image uploads in ProductCreate component

---
+++ src/config/cloudinary.js
@@ -0,0 +1,9 @@
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
+
+// Configure your Cloudinary instance
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: "dce1fl8qu", // Replace with your actual cloud name
+  },
+});
